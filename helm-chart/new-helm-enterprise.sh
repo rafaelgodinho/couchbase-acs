2a21edf319044f5fbd01bc1960eaf0cbb1731cb6
Rafael Godinho <rafael.godinho@microsoft.com>
2017-09-07 16:40:10 -0700

    Working version of postStart script

--- helm-chart/new-helm-enterprise.sh
+++ helm-chart/new-helm-enterprise.sh
@@ -2,8 +2,8 @@
 cd /mnt/c/Projects/Couchbase/CouchbaseACS/helm-chart/incubator/couchbase-enterprise
 
 #helm install --dry-run --name couchbase --namespace couchbase ./
-helm install --name couchbase --namespace couchbase-enterprise ./
+helm install --name couchbase-enterprise --namespace couchbase-enterprise ./
 watch -n 5 kubectl get all --namespace couchbase-enterprise
-#helm delete couchbase --purge
+#helm delete couchbase-enterprise --purge
 
 #kubectl exec couchbase-couchbase-0 --namespace couchbase-enterprise -i -t -- bash -il
